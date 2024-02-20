1412c963771fff357399a97ce41a599290b42692
shikha <shikhakumari3039@gmail.com>
2024-02-21 00:21:08 +0530

    Basic

---
+++ Java_Programming/Basic/assignment.java
@@ -0,0 +1,14 @@
+public class assignment {
+    public static void main(String[] args) {
+        int a=4;
+        int var;
+        var=a;
+        System.out.println("var using=:"+var);
+
+        var+=a;
+        System.out.println("var using+=:"+var);
+
+        var*=a;
+        System.out.println("var using*=:"+var);
+    }
+}
---
+++ Java_Programming/Command_Line_Argument/first.java
@@ -0,0 +1,16 @@
+package Command_Line_Argument;
+
+public class first {
+    public static void main(String args[]) {
+        int i;
+        float f;
+        double d;
+        System.out.println("Enter integer, float, double value:");
+        i=Integer.parseInt(args[0]);
+        System.out.println("i="+i);
+        f=Float.parseFloat(args[1]);
+        System.out.println("f="+f);
+        d=Double.parseDouble(args[2]);
+        System.out.println("d="+d);
+    }
+}
